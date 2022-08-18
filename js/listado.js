c91c1a7715f644282a73ab0be01131a831979cc0
Gianlu4 <gianlucacapno4@gmail.com>
2022-08-18 00:57:45 -0300

    agregue imagenes

--- js/listado.js
+++ js/listado.js
@@ -5,22 +5,23 @@ let categoriesArray = [];
 //función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
 function showCategoriesList(array){
     let htmlContentToAppend = "";
-
+    
     for(let i = 0; i < array.length; i++){ 
-        let category = array[i];
+        let products = array[i];
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1">
-                        <h4>`+ category.name +`</h4> 
-                        <p> `+ category.description +`</p> 
+                        <h4>`+ products.name + " - " + products.currency + " " + products.cost +`</h4> 
+                        
+                        <p> `+ products.description +`</p> 
                         </div>
-                        <small class="text-muted">` + category.productCount + ` artículos</small> 
+                        <small class="text-muted">` + products.soldCount + ` artículos</small> 
                     </div>
 
                 </div>
@@ -45,8 +46,9 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(LIST_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            categoriesArray = resultObj.data;
+            categoriesArray = resultObj.data.products;
             showCategoriesList(categoriesArray);
+            
         }
     });
 });
